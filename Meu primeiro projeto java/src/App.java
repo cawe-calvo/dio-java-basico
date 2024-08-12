a1def0cff09f2256f6892c0490f8bf88a283759c
cawe-calvo <cawe.calvo@gmail.com>
2024-08-12 16:22:44 -0300

    Alteração

--- Meu primeiro projeto java/src/App.java
+++ Meu primeiro projeto java/src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public static void main(String[] args) {
+    String primeiroNome = "Cawe";
+    String segundoNome = "Calvo";
+    
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat("").concat(segundoNome);
+}
+
 }
